5871be059dda681951de06e869283fb7aec4a1ea
Letícia Pires <leticiadppires@gmail.com>
2026-04-26 18:33:36 -0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from fastapi.testclient import TestClient
 from main import app
+
 
 client = TestClient(app)
 
